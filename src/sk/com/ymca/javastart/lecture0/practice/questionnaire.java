e0ee5953af985f87a4cb583786dfd77105d74145
Yevhenii Kladberov <kladberovv@gmail.com>
2024-03-23 15:31:27 +0100

    Classwork

---
+++ src/sk/com/ymca/javastart/lecture0/practice/questionnaire.java
@@ -0,0 +1,11 @@
+package sk.com.ymca.javastart.lecture0.practice;
+
+public class questionnaire {
+    public static void main(String[] args) {
+        String name = "Evgeniy", surname = "Kladberov", what_are_you = "Student";
+        char myMerch = '#';
+        boolean i_Am_Women = false;
+        System.out.println(name = name + "," + surname + "," + what_are_you + "\n" + myMerch + "\n" + i_Am_Women);
+    }
+
+}
